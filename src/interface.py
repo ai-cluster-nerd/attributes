4649c19847f12f5c7c6521bd55f7d9ad120a27bd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-06 03:26:27 +0100

    Update src/interface.py

--- src/interface.py
+++ src/interface.py
@@ -1,3 +1,4 @@
+"""Module interface.py"""
 import src.data.interface
 import src.elements.master as mr
 import src.elements.s3_parameters as s3p
@@ -5,14 +6,23 @@ import src.preface.interface
 
 
 class Interface:
+    """
+    Reads-in FEW NERD (Few-Shot Named Entity Recognition Data)
+    """
 
     def __init__(self):
-
+        """
+        Constructor
+        """
 
         self.__s3_parameters: s3p
         _, self.__s3_parameters, _ = src.preface.interface.Interface().exc()
 
     def exc(self) -> mr.Master:
+        """
+
+        :return:
+        """
 
         # Get data & labels
         master: mr.Master = src.data.interface.Interface(
